a2d9330e8428846e82c0db921b5378a869bbb10b
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-16 16:29:47 +0530

    initial commit

--- client/src/components/NavBar2.jsx
+++ client/src/components/NavBar2.jsx
@@ -138,7 +138,7 @@ function Navbar2 ({fetchBoards}) {
                       <span><strong>
                         {inv.Board.name}
                         </strong></span>
-                        <div className="d-flex justify-content-sm-end mt-1">
+                        <div className="d-flex justify-content-center mt-1">
 
                         <button className="btn btn-sm btn-success me-2" onClick={() => respondToInvitation(inv.id, 'accepted')}>Accept</button>
                         <button className="btn btn-sm btn-danger" onClick={() => respondToInvitation(inv.id, 'declined')}>Reject</button>
