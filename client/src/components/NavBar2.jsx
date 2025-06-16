dde67909b505b2d07f3b028d722f879fb7049e36
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-16 11:42:57 +0530

    initial commit

--- client/src/components/NavBar2.jsx
+++ client/src/components/NavBar2.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 function Navbar2 () {
   const [invitations, setInvitations] = useState([]);
   // Function to handle logout action
