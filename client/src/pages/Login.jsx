ed52b1602a939bf99c24f92cb1cbaab283b4a20c
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-17 14:37:26 +0530

    initial commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -162,8 +162,8 @@ const handlePasswordReset = async (e) => {
         className="position-fixed top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center"
         style={{ zIndex: 9999}}
       >
-        <div className="card p-4 rounded-4" style={{width: "350px", background: "#fff"}}>
-          <h4 className="mb-3 text-center" style={{color: "#2c3e50", background: "linear-gradient(to bottom,rgb(244, 190, 190), #F0E68C)"}}>Reset Password</h4>
+        <div className="card p-4 rounded-4" style={{width: "350px", background: "linear-gradient(to bottom,rgb(244, 190, 190), #F0E68C)"}}>
+          <h4 className="mb-3 text-center" style={{color: "#2c3e50", }}>Reset Password</h4>
           <form onSubmit={(handlePasswordReset)}>
             <div className="mb-3">
               <label>Email</label>
@@ -186,12 +186,12 @@ const handlePasswordReset = async (e) => {
                 />
 
             </div>
-            <button type="submit" className="btn w-100 mb-2" style={{background: 'linear-gradient(to bottom right, #2c3e50, #4a6b8c)'}}>
+            <button type="submit" className="btn w-100 mb-2" style={{background: 'linear-gradient(to bottom right, #2c3e50, #4a6b8c)', color: 'white'}}>
               Reset
             </button>
             <button 
             type="button"
-            style={{background: 'linear-gradient(to bottom right, #2c3e50, #4a6b8c)'}}
+            style={{background: 'linear-gradient(to bottom right, #2c3e50, #4a6b8c)',color: 'white'}}
             className="btn w-100"
             onClick={() => setShowPopup(false)}
             >
