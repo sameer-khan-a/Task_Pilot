778413ca203f98c6634bcc2e28f77eb0aec01d6e
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-17 10:20:02 +0530

    initial commit

--- client/src/components/NavBar2.jsx
+++ client/src/components/NavBar2.jsx
@@ -50,7 +50,7 @@ function Navbar2 ({fetchBoards}) {
     <nav
       className="navbar navbar-expand-lg navbar-dark fixed-top shadow"
       style={{
-        background: 'linear-gradient(to bottom right,rgb(77, 97, 116),rgb(31, 69, 106))',
+        background: 'linear-gradient(to top left, #2c3e50, #3a5069)',
         backgroundSize: 'cover',
         zIndex: 1000,
         paddingLeft: '1rem',
--- client/src/components/Navbar1.jsx
+++ client/src/components/Navbar1.jsx
@@ -4,7 +4,7 @@ function Navbar1() {
     <nav
       className="navbar navbar-expand-lg navbar-dark fixed-top shadow"
       style={{
-         background: 'linear-gradient(to bottom right,rgb(77, 97, 116),rgb(31, 69, 106))',
+         background: 'linear-gradient(to top left, #2c3e50, #3a5069)',
         backgroundSize: 'cover',
         zIndex: 1000,
         paddingLeft: '1rem',
