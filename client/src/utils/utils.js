87d7f4a09e23845ed5cc932c86eac76cca0a594e
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-18 13:58:16 +0530

    initial commit

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -3,5 +3,7 @@ export const groupTasksByStatus = (tasks) => {
         todo: tasks.filter(task => task.status === 'todo'),
         inprogress: tasks.filter(task => task.status === 'inprogress'),
         done: tasks.filter(task => task.status === 'done'),
+        onhold: tasks.filter(task => task.status === 'onhold'),
+        pinned: tasks.filter(task => task.status === 'pinned'),
     };
 };
