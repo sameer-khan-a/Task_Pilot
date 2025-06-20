eff9029fbf28d72d43b71e7245caba915a0c7641
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-20 16:39:32 +0530

    initial commit

--- client/src/components/NavBar2.jsx
+++ client/src/components/NavBar2.jsx
@@ -43,7 +43,7 @@ const markNotificationAsRead = async (notificationId) => {
         await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/notifications/${notificationId}/read`, null,  {
             headers: {Authorization: `Bearer ${token}`},
         });
-
+        fetchTaskNotifications();
         setNotifications(prev => 
             prev.map(n=> n.id === notificationId ? {...n, read: true}: n)
         );
