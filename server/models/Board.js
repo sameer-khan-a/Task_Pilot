3af8c6e93af15f3d68ff907e0df4af18b5adf532
sameer-khan-a <sameerkhan003a@gmail.com>
2025-06-23 15:07:59 +0530

    initial commit

--- server/models/Board.js
+++ server/models/Board.js
@@ -3,31 +3,38 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
 
 // Define the Board model with fields: name, description, and createdBy (user ID)
-const Board = sequelize.define('Board', {
-  
-  // Name of the board - required field
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+const Board = sequelize.define(
+  'Board',
+  {
+    // Name of the board - required field
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-  // Optional description of the board
-  description: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
+    // Optional description of the board
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
 
-  // ID of the user who created/owns the board
-  createdBy: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0, // Placeholder or fallback, ideally should be set explicitly
+    // ID of the user who created/owns the board
+    createdBy: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users', // Make sure table name is correct in DB
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'NO ACTION',
+    },
+  },
+  {
+    // Model options (e.g., timestamps, tableName)
+    timestamps: true,
   }
-
-}, {
-  // Automatically adds createdAt and updatedAt timestamps
-  timestamps: true,
-});
+);
 
 // Export the Board model so it can be used in other parts of the application
 module.exports = Board;
